2df87a2c063789773532d16b33f3f10f6898c85b
VeraGritcu <vera.gritcu@gmail.com>
2020-01-12 17:10:50 +0200

    upload

---
+++ UserPosts/UserPosts.App/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UserPosts.Data;
+using UserPosts.Domain;
+using UserPosts.Services;
+
+namespace UserPosts.App
+{
+    public class Program
+    {
+        delegate void Printer();
+        static void Main()
+        {
+            //List<Printer> printers = new List<Printer>();
+            //int i = 0;
+            //for (; i < 10; i++)
+            //{
+            //    printers.Add(delegate { System.Console.WriteLine(i); });
+            //}
+
+            //foreach (var printer in printers)
+            //{
+            //    printer();
+            //}
+
+            int n = int.Parse(Console.ReadLine());
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    if (row <= 9 && col == 1)
+                    {
+                        Console.Write(" ");
+                    }
+                    else if (col >= 9)
+                    {
+                        Console.Write(col);
+                    }
+                    else
+                    {
+                        Console.Write(col + " ");
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ UserPosts/UserPosts.Services.UnitTests/UserServiceTests.cs
@@ -0,0 +1,103 @@
+﻿namespace UserPosts.Services.UnitTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using NSubstitute;
+    using UserPosts.Domain;
+
+    [TestFixture]
+    public class UserServiceTests
+    {
+        private IUserRepository fakeUserRepository;
+        private User fakeUser;
+        private List<Post> dummyPosts;
+
+        [SetUp]
+        public void Setup()
+        {
+            fakeUserRepository = Substitute.For<IUserRepository>();
+
+            fakeUser = new User()
+            {
+                Email = "fake@fake.com",
+                Id = 1,
+                Name = "name",
+                Username = "username"
+            };
+
+            dummyPosts = new List<Post>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                dummyPosts.Add(new Post()
+                {
+                    UserId = 1,
+                    Body = "asdsa"
+                });
+            }
+        }
+
+        [Test]
+        public void Should_Have_Inactive_Status_When_Less_Than_Five_Posts()
+        {
+            //Arrange
+
+            FakeUserRepository fakeUserRepository = new FakeUserRepository();
+            FakePostRepository fakePostRepository = new FakePostRepository();
+            UserService sut = new UserService(fakeUserRepository, fakePostRepository);
+            //Act
+
+            var response = sut.GetUserActiveRespose(1);
+
+            //Assert
+            Assert.AreEqual(UserPostsStatus.Inactive, response.Status);
+        }
+
+        [Test]
+        public void Should_Have_Inactive_Status_When_Less_Than_Five_Posts2()
+        {
+            //Arrange
+
+            fakeUserRepository.GetById(1).Returns(fakeUser);
+
+            var fakePostRepository = Substitute.For<IPostRepository>();
+
+            var fakePosts = dummyPosts.Take(4).ToList();
+
+            fakePostRepository.GetPostsByUserId(1).Returns(fakePosts);
+
+            UserService sut = new UserService(fakeUserRepository, fakePostRepository);
+            //Act
+
+            var response = sut.GetUserActiveRespose(1);
+
+            //Assert
+            Assert.AreEqual(UserPostsStatus.Inactive, response.Status);
+        }
+
+        [Test]
+        public void Should_Have_Inactive_Status_When_Less_Than_Five_Posts3()
+        {
+            //Arrange
+            fakeUserRepository.GetById(1).Returns(fakeUser);
+
+            var fakePostRepository = Substitute.For<IPostRepository>();
+
+            var fakePosts = dummyPosts.Take(7).ToList();
+
+            fakePostRepository.GetPostsByUserId(1).Returns(fakePosts);
+
+            UserService sut = new UserService(fakeUserRepository, fakePostRepository);
+            //Act
+
+            var response = sut.GetUserActiveRespose(1);
+
+            //Assert
+            Assert.AreEqual(UserPostsStatus.Active, response.Status);
+        }
+    }
+}
